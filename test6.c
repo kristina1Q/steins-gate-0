d090239329f16615743ef2b88581005dfed3bf99
kristina1Q <123304884+kristina1Q@users.noreply.github.com>
2024-07-09 09:41:19 +0800

    Add files via upload

---
+++ test6.c
@@ -0,0 +1,20 @@
+/*#include <stdio.h>
+int main()
+{
+    printf("%s\n", "wonderful");
+    return 0;
+} */
+
+#include <stdio.h>
+int main()
+{
+    int ch= 10
+    ;if (ch>=5)
+    {
+        printf("成功");
+    }
+    return 0;
+}
+//>= 大于或等于
+//== 等于  而非= 赋值
+//10==a 可以避免等于弄成= 赋值的错误
